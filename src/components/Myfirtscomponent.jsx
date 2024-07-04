3963f744e727a06d18adbe84badff660e52c1a16
Javier Uribe <javieruribt@gamail.com>
2024-07-04 01:13:07 -0500

    My First Component

---
+++ src/components/Myfirtscomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Myfirtscomponent = () => {
+  return (
+    <>
+        <div>Myfirstcomponent</div>
+        <div>Hello world, I'm Javier Uribe Trujillo</div>
+        <div>27 de junio de 2024</div>
+    </>
+    
+  )
+}
+
